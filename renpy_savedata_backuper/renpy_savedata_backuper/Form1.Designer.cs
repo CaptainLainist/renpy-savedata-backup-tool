7ed352cb09f07d35a8d410cfcfd6a07dff78b890
CaptainLainist <52608597+CaptainLainist@users.noreply.github.com>
2023-05-07 13:18:46 +0200

    el programa

---
+++ renpy_savedata_backuper/renpy_savedata_backuper/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace renpy_savedata_backuper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Black;
+            this.listBox1.ForeColor = System.Drawing.Color.Red;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(24, 86);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(251, 329);
+            this.listBox1.TabIndex = 0;
+            // 
+            // saveButton
+            // 
+            this.saveButton.BackColor = System.Drawing.Color.Black;
+            this.saveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.ForeColor = System.Drawing.Color.Red;
+            this.saveButton.Location = new System.Drawing.Point(281, 86);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(217, 58);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "save data";
+            this.saveButton.UseVisualStyleBackColor = false;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // loadButton
+            // 
+            this.loadButton.BackColor = System.Drawing.Color.Black;
+            this.loadButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loadButton.ForeColor = System.Drawing.Color.Red;
+            this.loadButton.Location = new System.Drawing.Point(281, 150);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(217, 58);
+            this.loadButton.TabIndex = 2;
+            this.loadButton.Text = "load data";
+            this.loadButton.UseVisualStyleBackColor = false;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Cyan;
+            this.label1.Location = new System.Drawing.Point(21, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(505, 39);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Renpy SaveData Backup Tool";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(553, 447);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ renpy_savedata_backuper/renpy_savedata_backuper/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace renpy_savedata_backuper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //global variables
+        string[] folders;
+        string path;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //get renpy savedata path
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            path = Path.Combine(appDataPath, "RenPy");
+            folders = Directory.GetDirectories(path);
+            
+            //add folders to listBox
+            foreach (string folder in folders)
+            {
+                listBox1.Items.Add(Path.GetFileName(folder));
+            }
+
+
+            
+        }
+
+        //save backup
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+
+            if (listBox1.SelectedIndex != -1)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Zip files (*.zip)|*.zip";
+                saveFileDialog.Title = "Save Backup File";
+                saveFileDialog.FileName = listBox1.Text;
+                saveFileDialog.FileOk += SaveFileDialog_FileOk;
+                saveFileDialog.ShowDialog();
+            }
+            else {
+                MessageBox.Show("ERROR: Select a Game to save it's data");
+            }
+            
+            
+
+
+        }
+
+        //save backup when pressed OK
+        private void SaveFileDialog_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = (SaveFileDialog)sender;
+            string fileName = saveFileDialog.FileName;
+
+            string folder = Path.Combine(path, listBox1.Text);
+            
+            
+            try {
+                ZipFile.CreateFromDirectory(folder, fileName);
+            } catch (System.IO.IOException) {
+                File.Delete(fileName);
+                ZipFile.CreateFromDirectory(folder, fileName);
+            }
+
+            MessageBox.Show("Backup Saved Correctly");
+        }
+
+
+
+        //load backup game
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+
+            if (listBox1.SelectedIndex != -1)
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "Zip Files (*.zip)|*.zip";
+                openFileDialog.Title = "Open Backup File";
+                openFileDialog.ShowDialog();
+
+                if (openFileDialog.FileName != "")
+                {
+                    string output_folder = Path.Combine(path, listBox1.Text);
+
+                    try
+                    {
+                        ZipFile.ExtractToDirectory(openFileDialog.FileName, output_folder);
+                    }
+                    catch (System.IO.IOException)
+                    {
+                        string[] files = Directory.GetFiles(output_folder);
+                        foreach (string file in files)
+                        {
+                            File.Delete(file);
+                        }
+                        ZipFile.ExtractToDirectory(openFileDialog.FileName, output_folder);
+                    }
+
+                    MessageBox.Show("Backup loaded correctly");
+                }
+            }
+            else {
+
+                MessageBox.Show("ERROR: Select a Game to load it's data");
+
+            }
+            
+
+        }
+
+       
+    }
+}
